dbbcbab2ab9111ec6461cfa67a6940a3385dfcd0
hassan moradnezhad <hsn.moradnezhad@gmail.com>
2024-04-23 02:09:09 +0330

    feat:add single-product page

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,22 @@
+import { Container } from "@component/container";
+import { apiProduct } from "@/api/products";
+import React, { Suspense } from "react";
+import { ProductItem } from "@component/product-item";
+
+interface ProductPageParamsProps {
+  params: { productId: string | undefined };
+}
+
+export default async function ProductPage(props: ProductPageParamsProps) {
+
+  const productId = props.params.productId;
+  const product = await apiProduct.getById(productId).fetcher();
+
+  return (
+    <Container>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ProductItem product={product?.response}/>
+      </Suspense>
+    </Container>
+  );
+}
